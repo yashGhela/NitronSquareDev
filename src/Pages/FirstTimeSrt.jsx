597bfad07e50285033cc4eba95dbbeb075a4e56c
yashGhela <ghelayash@gmail.com>
2022-11-15 17:15:51 +0200

    Sessions collection made on account create

--- src/Pages/FirstTimeSrt.jsx
+++ src/Pages/FirstTimeSrt.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import './FirstTimeSrt.css';
 import { useLocation } from 'react-router-dom';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../firebaseConfig';
 
 function FirstTimeSrt() {
     let location = useLocation();
-    console.log(location.state.user);
+    const user= location.state.user;
+    
+    
 
     const [newSubject, setNewSubject] = useState();
     const[subjectError, setSubjectError] = useState(null);
+
+    const addSubject=async()=>{
+      await addDoc(collection(db, 'Users', user, 'Sessions', newSubject),{place:'first doc'});
+    }
   return (
-    <div>
+    <div className='Cont'>
         <h1>Lets Add Some Subjects!</h1>
-        <input type="text" placeholder='Accounting' />
-        <button >Add!</button>
+        <input type="text" placeholder='Accounting'  onChange={(e)=>{setNewSubject(e.target.value)}}/>
+        <button className='Gobtn1' onClick={addSubject}>Add!</button>
     </div>
   )
 }
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -3,18 +3,25 @@ import './Login.css';
 import {auth, db, provider} from '../firebaseConfig';
 import {signInWithPopup} from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
-import {doc, getDoc, setDoc} from 'firebase/firestore';
+import {addDoc, collection, doc, getDoc, setDoc} from 'firebase/firestore';
 
 function SignUp() {
 
   let nav= useNavigate();
+  const createSes=async({user})=>{
+     await addDoc(collection(db,'Users',user,'Sessions'), {place: 'placeholder'}); /*this only worked once when i assigned a name to the document, so what i need to do is find a way to 
+     get the id then pass it thru*/
+     
+      
+  }
 
-  const signUp=()=>{
+  const signUp=()=>{  //to put it simpy once a user signs up they'll be added to the database and then sent to add more subjects to sessions
     signInWithPopup(auth, provider).then(async(result)=>{
       const ref = doc(db, 'Users', result.user.uid)
       const docRef = setDoc(ref, {username: result.user.displayName,tier: 'free'});
       nav('/FirstTimeSrt', {state: {user: result.user.uid}});
       localStorage.setItem('isAuth', true);
+      createSes({user: result.user.uid})
       
       
     })
