b45ceafaf938b78de8a237020e99751c575eeafc
yashGhela <ghelayash@gmail.com>
2022-11-25 18:06:11 +0200

    Changed timer to only run once

--- src/Util/Timer.jsx
+++ src/Util/Timer.jsx
@@ -32,6 +32,8 @@ function Timer() {
    //Timer code
    
    const settingsInfo = location.state;
+   const workSeconds = settingsInfo.workMinutes*60;
+   const breakSeconds= settingsInfo.breakMinutes*60;
 
    const [isPaused, setIsPaused]= useState(false); //checks if paused or not
    const [mode, setMode] = useState('work')//work,break, pause
@@ -41,32 +43,40 @@ function Timer() {
    const isPausedRef = useRef(isPaused);
    const modeRef = useRef(mode);
    
-   const totalSeconds = mode==='work'? //total seconds is = to which ever mode chosen
-   settingsInfo.workMinutes*60 //multiplies the workminutes by 60 to become actual minutes
-   : settingsInfo.breakMinutes*60 //multiplies the breakminutes by 60 to become actual minutes
+   let totalSeconds;
+   if (mode==='work'){
+     totalSeconds=workSeconds
+   }if(mode==='break'){
+     totalSeconds=breakSeconds
+   }
    
 
    function initTimer(){ //initializes the timer
-    secondsLeftRef.current= settingsInfo.workMinutes*60; //current seconds left is = to work minutes
+    secondsLeftRef.current= workSeconds; //current seconds left is = to work minutes
     setSecondsLeft(secondsLeftRef.current); //sets the seconds left to the the current ref
 
    }
 
    function switchMode(){
-    const nextMode = modeRef.current ==='work'?'break':'work'; //first mode is work then break then back to work
-    const nextSeconds = (nextMode==='work'?  //decides how to determine multiplaction of mode minutes
-    settingsInfo.workMinutes:
-    settingsInfo.breakMinutes)*60
-    setMode(nextMode);//sets mode to the next mode 
-    modeRef.current= nextMode;
-    setSecondsLeft(nextSeconds);//sets seconds to next seconds
-    secondsLeftRef.current = nextSeconds;
+
+       setMode('break');
+       modeRef.current='break';
+      setSecondsLeft(breakSeconds);
+      secondsLeftRef.current= breakSeconds;
+    
+
+    
+//sets mode to the next mode 
+    
+   
     play();
    }
 
    function tick(){ 
-    secondsLeftRef.current--;//minuses by 1 each time
-    setSecondsLeft(secondsLeftRef.current);
+   
+      secondsLeftRef.current--;//minuses by 1 each time
+      setSecondsLeft(secondsLeftRef.current);
+    
    }
 
    useEffect(()=>{
@@ -74,19 +84,18 @@ function Timer() {
     const interval =setInterval(()=>{
       if (isPausedRef.current){  //if paused nothing happens
         return;
-      }if(secondsLeftRef.current===0){ //if its at 0 switch the mode
-        switchMode();
-        play();
-        setIsPaused(true);
-        isPausedRef.current=true
+      }if(modeRef.current==='work' && secondsLeftRef.current===0){ //if its at 0 switch the mode
+        switchMode()
+        
+      }if(modeRef.current==='break'&&secondsLeftRef.current===0){
         
       }
       tick(); //ticks
-    }, 1000);//timeout is 1000 go, activates how much should be minused by
+    }, 10);//timeout is 1000 go, activates how much should be minused by
     return ()=>clearInterval(interval); //clears the interval
    },  [settingsInfo]);
 
-   const percentage = Math.round(secondsLeft/totalSeconds *100); //rounds the number 
+ const percentage = Math.round(secondsLeft/totalSeconds *100); //rounds the number 
  const minutes = Math.floor(secondsLeft/60); 
  let seconds = secondsLeft%60;
 
