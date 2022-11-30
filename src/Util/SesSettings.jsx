2687710835e2ebbb9d20b3d3044570e4799aec82
yashGhela <ghelayash@gmail.com>
2022-11-30 15:30:45 +0200

    adding more subjects works, selector broken

--- src/Util/SesSettings.jsx
+++ src/Util/SesSettings.jsx
@@ -4,8 +4,8 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ReactSlider from 'react-slider';
 import './SesSettings.css';
-import { Button } from 'react-bootstrap';
-import {doc,  getDoc} from 'firebase/firestore'
+import { Button, Form, Modal } from 'react-bootstrap';
+import {arrayUnion, doc,  getDoc, updateDoc} from 'firebase/firestore'
 import {db} from '../firebaseConfig';
 
 
@@ -17,11 +17,13 @@ function SesSettings() {
   const [breakMinutes, setBreakMinutes] = useState(15);//sets break minutes
   const [subject, setSubject] = useState('');//sets the subject for the user
   const [isChecked, setIsChecked]=useState(false); 
+  const [modalShow, setModalShow]= useState(false);
   const [isCheckederr, setIsCheckederr]=useState('');
   const [disabled, setDisabled]=useState(true);
   const [subjectList, setSubjectList] =useState([]);
-  
-  
+  const [subj, setSub]=useState('');
+  const [disabledM, setDisabledM]= useState(true)
+  const [suberr, setSuberr]=useState(null);
   const user= sessionStorage.getItem('useraidt');
   const subref=doc(db,'Users',user,'Sessions','SubjectsList')
   
@@ -41,6 +43,13 @@ function SesSettings() {
 
     
   })
+
+  const newSub=async()=>{
+    await updateDoc(subref, {
+      subjects: arrayUnion(subj)
+    });
+    setModalShow(false);
+  }
  
   
 
@@ -51,17 +60,17 @@ function SesSettings() {
    
      
      docSnap();
-     setDisabled(formValidation());
+    formValidation()
     
   },[])
 
   const formValidation = () =>{  //checks for errors
-    if (isChecked ===false){
-      setIsCheckederr('Subject cannot be blank');
+    if (subj===''){
+      setSuberr('Subject cannot be blank');
       return true
 
     }else{
-      setIsCheckederr(null);
+      setSuberr(null);
       return false
     }}
   
@@ -101,13 +110,12 @@ function SesSettings() {
       <div className="SubjectChoices">
         <h1>Pick A Subject:</h1>
         {subjectList.map((sub)=>{
-           if (sub===""){ 
-            <h3>No subjects</h3>}
+          
 
             return(
               <div className="list">
                
-              <input type="checkbox" value={sub} onClick={(e)=>{setSubject(e.target.value); if(e.target.checked){setIsChecked(true)} else{setIsChecked(false)}}} style={{marginRight:'5px'}}/>
+              <input type="checkbox" value={sub} onClick={(e)=>{setSubject(e.target.value); if(e.target.checked){setDisabled(false)} else{setDisabled(true)}}} style={{marginRight:'5px'}}/>
               <label>{sub}</label><br/>
             
               </div>
@@ -115,8 +123,32 @@ function SesSettings() {
           
           })}
         {isCheckederr&&<h3 style={{color:'red'}}>Please choose a subject</h3>}
-        <Button>Add a Subject</Button>
+        <Button onClick={()=>{setModalShow(true)}}>Add a Subject</Button>
       </div>
+
+      <Modal
+       show={modalShow}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      onHide={()=>setModalShow(false)}
+      centered>
+      <Modal.Header closeButton>
+        <Modal.Title  id="contained-modal-title-vcenter">
+          Please Add Your First Subject
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Label>Subject Name:</Form.Label>
+          <Form.Control placeholder='Algebra' onChange={(e)=>{setSub(e.target.value);if(e.target.value===''){setDisabledM(true)} else{setDisabledM(false)}}} style={{marginBottom:'20px'}}/>
+          <Button onClick={newSub} disabled={disabledM} style={{marginBottom:'20px'}}>Let's Go!</Button>
+        {suberr&&<h3 style={{color:'red', fontSize:'14px'}}>Please input a subject name</h3>}
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+       <h4>Don't worry, you can add more subjects later 🙂</h4>
+      </Modal.Footer>
+    </Modal>
 
   
 
