490aeb8dba0f60e409bcdd823d32043cf73132b4
yashGhela <ghelayash@gmail.com>
2022-12-07 20:28:49 +0200

    weird 10000 mui errors ting fixed, upload broke

--- src/Util/CreateScope.jsx
+++ src/Util/CreateScope.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
-import { Container, Form, Navbar } from 'react-bootstrap'
+import { addDoc, collection } from 'firebase/firestore';
+import React, { useState } from 'react'
+import { Container, Form, Navbar, Button } from 'react-bootstrap'
+import {db} from '../firebaseConfig';
+import Cookies from 'universal-cookie';
+import { useNavigate } from 'react-router-dom';
+
 
 function CreateScope() {
+
+  const cookie = new Cookies()
+  const user=cookie.get('useraidt')
+  const nav=useNavigate();
+ 
+
+  const [title, setTitle]=useState();
+  const [description, setDescritption]=useState('');
+  
+  const [disabled, setDisabled]=useState(true);
+  const [task, setTask]=useState('');
+  const [taskList, setTaskList]= useState([]);
+
+  const addScope=async()=>{
+    const subref= collection(db,'Users',user,'Scopes');
+    await addDoc(subref, {
+      title: title,
+      description: description,
+      tasks: [taskList]
+    })
+    nav(`/Dashboard/${user}`)
+  }
+
   return (
     <div className='Page2'>
         <Navbar bg='dark' variant='dark'>
@@ -18,13 +46,20 @@ function CreateScope() {
           <Form style={{width:'500px'}}> 
             <Form.Group>
               <Form.Label >Title:</Form.Label>
-              <Form.Control/>
+              <Form.Control onChange={(e)=>{setTitle(e.target.value)}}/>
               <Form.Label>Description:</Form.Label>
-              <Form.Control as='textarea' rows={3} style={{resize:'none'}} />
+              <Form.Control as='textarea' rows={3} style={{resize:'none'}} onChange={(e)=>{setDescritption(e.target.value)}} />
+              <Form.Label>Add a Task</Form.Label>
+              <Form.Control onChange={(e)=>{setTask(e.target.value)}} style={{marginTop:'5px'}} value={task}/>
+              <Button onClick={()=>{setTaskList([...taskList,task]);console.log(taskList);setTask(''); <h2>Added!</h2>}} style={{marginTop:'10px'}} >Add</Button>
               
             </Form.Group>
           </Form>
+
+          <Button onClick={()=>{setTaskList([...taskList,task]); addScope()}}>Finish</Button>
           </div>
+
+        
         
     </div>
   )
