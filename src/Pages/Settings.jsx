d427822b27e5c8e8aae1c02ee4be44b5feaaba62
yashGhela <ghelayash@gmail.com>
2023-01-16 17:17:44 +0200

    various changes, log out works, sub delete not

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -5,8 +5,9 @@ import GooglePayButton from '@google-pay/button-react'
 import {arrayRemove, doc,getDoc, updateDoc} from 'firebase/firestore'
 import Cookies from 'universal-cookie'
 import { useState } from 'react'
-import { db } from '../firebaseConfig'
+import { db,auth } from '../firebaseConfig'
 import {Speedometer,CardText,BarChart, Hr, Journals, Bullseye, Instagram } from 'react-bootstrap-icons'
+import {signOut} from 'firebase/auth';
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import { async } from '@firebase/util'
@@ -54,6 +55,7 @@ function Settings() {
 
 
     const DeleteSub=async(sub)=>{
+        let subref=  doc(db,'Users',user,'Subjects','SubjectsList');
         await updateDoc(subref,{
             subjects: arrayRemove(sub)
         })
@@ -89,46 +91,7 @@ function Settings() {
                     <Card.Text>
                         Join the Improvr family now and gain unlimited access to all features.
                     </Card.Text>
-                    <GooglePayButton
-                    environment='TEST'
-                    buttonSizeMode='fill'
-                    style={{width: '200px'}}
-                    paymentRequest={{
-                        apiVersion:2,
-                        apiVersionMinor: 0,
-                        allowedPaymentMethods:[
-                            {
-                                type:'CARD',
-                                parameters:{
-                                    allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
-                                    allowedCardNetworks: ['MASTERCARD', 'VISA']
-                                },
-                                tokenizationSpecification:{
-                                    type:'PAYMENT_GATEWAY',
-                                    parameters:{
-                                        gateway:'example',
-                                        gatewayMerchantId: 'exampleGatewayMerchantId'
-                                    }
-                                }
-                            }
-                        ],
-                        merchantInfo:{
-                            merchantId: 'BCR2DN4TYSUMTD3M',
-                            merchantName: 'Demo Only'
-
-                        },
-                        transactionInfo:{
-                            totalPriceStatus:'FINAL',
-                            totalPriceLabel: 'Total',
-                            totalPrice:'5',
-                            currencyCode:'USD',
-                            countryCode:'US'
-                        }
-                    }}
-                    onLoadPaymentData={paymentData=>{
-                        console.log('SUCCESS!', paymentData.paymentMethodData)
-                    }}
-                    />
+                 <Button variant='light' style={{width:'180px'}}>Pay</Button>
                   
                 </Card.Body>
 
@@ -151,9 +114,9 @@ function Settings() {
                 type="checkbox"
                  value={sub} 
                  variant="secondary"
-                onClick={()=>{DeleteSub({sub:sub})}}
+                 onClick={()=>{DeleteSub({sub})}}
                
-                 style={{marginRight:'5px', marginBottom:'5px', width:'100px'}}>
+                style={{marginRight:'5px', marginBottom:'5px', width:'100px', height:'35px', cursor:'pointer', display:'flex',paddingBottom:'20px', paddingRight:'5px', paddingLeft:'5px', backgroundColor:'RGB(97,149,232)',color:'white'}}>
                   {sub}
                 </Card>
                 </Col>
@@ -170,25 +133,38 @@ function Settings() {
            <h2 style={{color:'gray', fontSize:'22px', marginTop:'20px', marginBottom:'10px'}}>Help:</h2>
             <h3 style={{color:'gray', fontSize:'16px', marginTop:'20px', marginBottom:'10px'}}>E-mail: info@nitrondigital.com</h3>
 
-           <Card  onClick={()=>{window.location.href="'https://www.instagram.com/nitrondigital/'"}} style={{backgroundColor:'rgb(41, 44, 51)',height:'50px', width:'50px', border:'none'}}>
+           <Card  onClick={()=>{window.location.href="'https://www.instagram.com/nitrondigital/'"}} style={{backgroundColor:'rgb(12, 12, 12)',height:'50px', width:'50px', border:'none'}}>
            <Instagram style={{color:'gray', height:'50px', width:'50px',cursor:'pointer'}}/>
            </Card>
 
+           <Card 
+           disabled={false}
+           onClick={LogOut}
+           
+           style={{marginRight:'5px', marginBottom:'5px', marginTop: '10px',width:'100px', height:'35px', cursor:'pointer', display:'flex',paddingBottom:'20px', paddingRight:'5px', paddingLeft:'5px', backgroundColor:'RGB(97,149,232)',color:'white'}}>
+            Log Out
+           </Card>
+
+       
+
            </div>
 
-           <Button onClick={LogOut}>Log Out</Button>
+          
 
 
           </Container>
+          
 
 
             
 
         </div>
+        
 
 
 
     </div>
+    
   )
 }
 
