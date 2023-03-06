65200f6340c81d2717ca093d52dd45f6d26ffdbb
yashGhela <ghelayash@gmail.com>
2023-03-06 16:55:58 +0200

    changed scopes to be beeegg

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -396,115 +396,119 @@ function Dashboard() {
           <Row >
           {scopesExists? scopeList.map((scop)=>{
             return(
-              <Col xs='2'  >
-                <div>
+             
+               <Col style={{width:'25%'}}xs='2' >
+                
 
-                 <Card style={{width:'100%', background:'#282b2e', color:'lightgray' , cursor:'pointer', height:'180px', marginTop:'10px',}} onClick={()=>{setScopeModalShow(true); setModalData(scop); }}>
-                    <Card.Body>
-                     <Card.Title>{scop.title}</Card.Title>
-                      <Card.Text>
+                <Card style={{width:'100%', background:'#282b2e', color:'lightgray' , cursor:'pointer', height:'100%', marginTop:'10px',}} onClick={()=>{setScopeModalShow(true); setModalData(scop); }}>
+                   <Card.Body>
+                    <Card.Title>{scop.title}</Card.Title>
+                    <Card.Text>
                        {scop.description}
                        </Card.Text>
-                     </Card.Body>
+                    </Card.Body>
 
-                    </Card>
-
-                  <Modal
-                    
-                    show={scopeModal}
-                      size="lg"
-                      aria-labelledby="contained-modal-title-vcenter"
-                      onHide={()=>{setScopeModalShow(false)}}
-                      className="special_modal"
-                      
-                      centered>
-                  <Modal.Header closeButton closeVariant='white'> 
-                  <Modal.Title  id="contained-modal-title-vcenter"  style={{marginRight:'70%'}}>
-                    Scope
-                  </Modal.Title>
-                  <Button variant='outline-light' onClick={()=>{if(isUpdate){setIsUpdate(false); setSaveDis(true)}else{setIsUpdate(true); setSaveDis(false)}}} style={{marginRight:'10px'}}>Edit</Button>
-                  <Button variant='danger' onClick={()=>{DeleteSco({id: modalData.id})}}>Delete</Button>
-                  </Modal.Header>
-                    <Modal.Body>
-                      
-                    {isUpdate? <Form style={{display:'flex',backgroundColor:'RGB(12,12,12)', padding:'10px', margin:'10px', borderRadius:'10px'}}>
-                        <Form.Control style={{width:'80%',}} placeholder={modalData.title} 
-                        onChange={(e)=>{if(!e.target.value){setUpdateTitle(modalData.title)}else{
-                          setUpdateTitle(e.target.value)
-                        }}}/>
-                        
-                      </Form>:<h1  style={{fontWeight:'bold', backgroundColor:'RGB(12,12,12)', padding:'10px', margin:'10px', borderRadius:'10px' ,color:'lightgray'}}>{modalData.title}</h1>}
+                   </Card>
+                   <Modal
+                   
+                   show={scopeModal}
+                     size="lg"
+                     aria-labelledby="contained-modal-title-vcenter"
+                     onHide={()=>{setScopeModalShow(false)}}
+                     className="special_modal"
                      
-                      {isUpdate? <Form style={{display:'flex',backgroundColor:'RGB(12,12,12)', padding:'10px', margin:'10px', borderRadius:'10px'}}>
-                        <Form.Control style={{width:'80%',}} placeholder={modalData.description}
-                        onChange={(e)=>{if(!e.target.value){setUpdateDesc(modalData.description)}else{
-                          setUpdateDesc(e.target.value)
-                        }}}/>
-                        
-                      </Form>:<p style={{fontWeight:'400', backgroundColor:'RGB(12,12,12)', padding:'10px', margin:'10px', borderRadius:'10px' ,fontWeight:'lighter', fontSize:'20px',color:'lightgray'}}>Description:<br/>{modalData.description} </p>}
-                      
-                      
-                      <div className="mbod">
+                     centered>
+                 <Modal.Header closeButton closeVariant='white'> 
+                 <Modal.Title  id="contained-modal-title-vcenter"  style={{marginRight:'70%'}}>
+                   Scope
+                 </Modal.Title>
+                 <Button variant='outline-light' onClick={()=>{if(isUpdate){setIsUpdate(false); setSaveDis(true)}else{setIsUpdate(true); setSaveDis(false)}}} style={{marginRight:'10px'}}>Edit</Button>
+                 <Button variant='danger' onClick={()=>{DeleteSco({id: modalData.id})}}>Delete</Button>
+                 </Modal.Header>
+                   <Modal.Body>
+                     
+                   {isUpdate? <Form style={{display:'flex',backgroundColor:'RGB(12,12,12)', padding:'10px', margin:'10px', borderRadius:'10px'}}>
+                       <Form.Control style={{width:'80%',}} placeholder={modalData.title} 
+                       onChange={(e)=>{if(!e.target.value){setUpdateTitle(modalData.title)}else{
+                         setUpdateTitle(e.target.value)
+                       }}}/>
+                       
+                     </Form>:<h1  style={{fontWeight:'bold', backgroundColor:'RGB(12,12,12)', padding:'10px', margin:'10px', borderRadius:'10px' ,color:'lightgray'}}>{modalData.title}</h1>}
+                    
+                     {isUpdate? <Form style={{display:'flex',backgroundColor:'RGB(12,12,12)', padding:'10px', margin:'10px', borderRadius:'10px'}}>
+                       <Form.Control style={{width:'80%',}} placeholder={modalData.description}
+                       onChange={(e)=>{if(!e.target.value){setUpdateDesc(modalData.description)}else{
+                         setUpdateDesc(e.target.value)
+                       }}}/>
+                       
+                     </Form>:<p style={{fontWeight:'400', backgroundColor:'RGB(12,12,12)', padding:'10px', margin:'10px', borderRadius:'10px' ,fontWeight:'lighter', fontSize:'20px',color:'lightgray'}}>Description:<br/>{modalData.description} </p>}
+                     
+                     
+                     <div className="mbod">
 
-                      <div className="Incomplete" style={{backgroundColor:'RGB(12,12,12)', padding:'10px', margin:'10px', borderRadius:'10px' ,color:'lightgray'}}>
-                        <h3>Incomplete:</h3>
-                        <div style={{display:'inline'}}>
-                           <Form style={{display:'flex', marginBottom:'10px'}}>
-                              <Form.Control className='special_modal' placeholder='Add a Task' style={{width:'80%'}} onChange={(e)=>{setNewTask(e.target.value)}}/>
-                              <Button style={{marginLeft:'10px'}} onClick={()=>{newTassk({id:modalData.id});modalData.incomplete.push(newTask)}}>Add</Button>
-                            </Form>
-                           </div>
-                  
+                     <div className="Incomplete" style={{backgroundColor:'RGB(12,12,12)', padding:'10px', margin:'10px', borderRadius:'10px' ,color:'lightgray'}}>
+                       <h3>Incomplete:</h3>
+                       <div style={{display:'inline'}}>
+                          <Form style={{display:'flex', marginBottom:'10px'}}>
+                             <Form.Control className='special_modal' placeholder='Add a Task' style={{width:'80%'}} onChange={(e)=>{setNewTask(e.target.value)}}/>
+                             <Button style={{marginLeft:'10px'}} onClick={()=>{newTassk({id:modalData.id});modalData.incomplete.push(newTask)}}>Add</Button>
+                           </Form>
+                          </div>
+                 
 
-                              {modalData.incomplete?.map((inc)=>{
-                        return(
-                          <div className="list">
+                             {modalData.incomplete?.map((inc)=>{
+                       return(
+                         <div className="list">
 
-                            <Button  variant="secondary" value={inc}  style={{marginRight:'5px', marginBottom:'5px'}} onClick={()=>{movetask({id:modalData.id, task:inc}); 
-                              var del= modalData.incomplete.indexOf(inc); 
-                              console.log(del);
-                              modalData.incomplete.splice(del,1);
-                               modalData.complete.push(inc)}}><Check/></Button>
-                            <label style={{marginBottom: '5px'}}>{inc}</label><br/>
+                           <Button  variant="secondary" value={inc}  style={{marginRight:'5px', marginBottom:'5px'}} onClick={()=>{movetask({id:modalData.id, task:inc}); 
+                             var del= modalData.incomplete.indexOf(inc); 
+                             console.log(del);
+                             modalData.incomplete.splice(del,1);
+                              modalData.complete.push(inc)}}><Check/></Button>
+                           <label style={{marginBottom: '5px'}}>{inc}</label><br/>
 
-                          </div >
-                        )
-                      })}
+                         </div >
+                       )
+                     })}
 
-                      </div>
-                        
+                     </div>
+                       
 
-                      <div className="Complete"  style={{backgroundColor:'RGB(12,12,12)', padding:'10px', margin:'10px', borderRadius:'10px' ,color:'lightgray'}}>
-                        <h3>Complete:</h3>
-                        {modalData.complete?.map((comp)=>{
-                        return(
-                          <div className="list">
+                     <div className="Complete"  style={{backgroundColor:'RGB(12,12,12)', padding:'10px', margin:'10px', borderRadius:'10px' ,color:'lightgray'}}>
+                       <h3>Complete:</h3>
+                       {modalData.complete?.map((comp)=>{
+                       return(
+                         <div className="list">
 
-                          
-                            <Button  variant="secondary" value={comp}  style={{marginRight:'5px', marginBottom:'5px'}} onClick={()=>{
-                              movetaskBack({id:modalData.id, task:comp}); 
-                              var del = modalData.complete.indexOf(comp);
-                              modalData.complete.splice(del,1)
-                               modalData.incomplete.push(comp)}}><Check/></Button>
-                            <label style={{marginBottom: '5px'}}>{comp}</label><br/>
-                          
-                            
+                         
+                           <Button  variant="secondary" value={comp}  style={{marginRight:'5px', marginBottom:'5px'}} onClick={()=>{
+                             movetaskBack({id:modalData.id, task:comp}); 
+                             var del = modalData.complete.indexOf(comp);
+                             modalData.complete.splice(del,1)
+                              modalData.incomplete.push(comp)}}><Check/></Button>
+                           <label style={{marginBottom: '5px'}}>{comp}</label><br/>
+                         
+                           
 
-                          </div >
-                        )
-                      })}
-                        </div>
+                         </div >
+                       )
+                     })}
+                       </div>
 
-                      </div>
-                    </Modal.Body>
-                    <Modal.Footer>
-                    <Button variant='outline-light' onClick={()=>{Save({id:modalData.id})}} disabled={saveDis}>Save</Button>
-                    </Modal.Footer>
-                    </Modal>
+                     </div>
+                   </Modal.Body>
+                   <Modal.Footer>
+                   <Button variant='outline-light' onClick={()=>{Save({id:modalData.id})}} disabled={saveDis}>Save</Button>
+                   </Modal.Footer>
+                   </Modal>
+                   </Col>
+                   
 
-                </div>
-              </Col>
+                 
 
+          
+            
+             
 
             )
           }): <h1 style={{color:'lightgray', textAlign:'center', fontSize:'25px'}}>No scopes added yet</h1>}
